fe1fd8723104166664052ee43c45ab51a388daff
subbu3012 <bhsubbu.1995@gmail.com>
2018-05-16 03:27:16 +0530

    Feat - Responsiveness is added to the login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,20 @@
-import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MediaMatcher } from '@angular/cdk/layout';
+
 
 @Component({
     selector: 'login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
-    providers: []
+    providers: [MediaMatcher]
 })
 export class LoginComponent implements OnInit {
+
+    mobileQuery: MediaQueryList;
+    private _mobileQueryListener: () => void;
+
 
     public loginFormGroup: FormGroup = new FormGroup({
         loginId: new FormControl('', Validators.minLength(2)),
@@ -22,17 +28,17 @@ export class LoginComponent implements OnInit {
         confirmPassword: new FormControl('', Validators.minLength(2))
     });
 
-
-
-    constructor(public rtr: Router) {
-
+    constructor(public rtr: Router, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+        this.mobileQuery = media.matchMedia('(max-width: 600px)');
+        this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+        this.mobileQuery.addListener(this._mobileQueryListener);
     }
 
     ngOnInit() {
     }
 
     ngOnDestroy(): void {
-
+        this.mobileQuery.removeListener(this._mobileQueryListener);
     }
 
     public logInToApp() {
