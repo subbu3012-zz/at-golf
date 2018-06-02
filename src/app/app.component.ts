6313506101efe91aa8288091e4937eec7022ec1e
subbu3012 <bhsubbu.1995@gmail.com>
2018-06-02 19:04:11 +0530

    Fix : Changes are done for service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -8,7 +10,19 @@ import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 })
 export class AppComponent {
     public loadingTimeout: any;
-    constructor(public rtr: Router) {
+    constructor(public rtr: Router, public updates: SwUpdate) {
+        updates.available.subscribe(event => {
+            console.log('current version is', event.current);
+            console.log('available version is', event.available);
+            if (false) {
+                // updates.activateUpdate().then(() => document.location.reload());
+            }
+        });
+        updates.activated.subscribe(event => {
+            console.log('old version was', event.previous);
+            console.log('new version is', event.current);
+        });
 
+        // interval(6 * 60 * 60).subscribe(() => updates.checkForUpdate())
     }
 }
